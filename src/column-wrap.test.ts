5f0f9bd69aa1bb24df5da6d95a8f2dea4b78ccd9
j- <j@skeoh.com>
2019-06-05 13:58:56 +1000

    Implement functionality

--- src/column-wrap.test.ts
+++ src/column-wrap.test.ts
@@ -55,3 +55,107 @@ it('wraps "AAA\\nBBB" at 2 cols', () => {
   const actual = columnWrap(input, columns);
   expect(actual).toBe(expected);
 });
+
+it('wraps "A\\s\\s\\sB" at 2 cols', () => {
+  const columns = 2;
+  const input = "A   B";
+  const expected = "A   \nB";
+  const actual = columnWrap(input, columns);
+  expect(actual).toBe(expected);
+});
+
+it('wraps "Hello world" at 1 cols', () => {
+  const columns = 1;
+  const input = "Hello world";
+  const expected = "H\ne\nl\nl\no \nw\no\nr\nl\nd";
+  const actual = columnWrap(input, columns);
+  expect(actual).toBe(expected);
+});
+
+it('wraps "Hello world" at 2 cols', () => {
+  const columns = 2;
+  const input = "Hello world";
+  const expected = "He\nll\no \nwo\nrl\nd";
+  const actual = columnWrap(input, columns);
+  expect(actual).toBe(expected);
+});
+
+it('wraps "Hello world" at 3 cols', () => {
+  const columns = 3;
+  const input = "Hello world";
+  const expected = "Hel\nlo \nwor\nld";
+  const actual = columnWrap(input, columns);
+  expect(actual).toBe(expected);
+});
+
+it('wraps "Hello world" at 4 cols', () => {
+  const columns = 4;
+  const input = "Hello world";
+  const expected = "Hell\no \nworl\nd";
+  const actual = columnWrap(input, columns);
+  expect(actual).toBe(expected);
+});
+
+it('wraps "Hello world" at 5 cols', () => {
+  const columns = 5;
+  const input = "Hello world";
+  const expected = "Hello \nworld";
+  const actual = columnWrap(input, columns);
+  expect(actual).toBe(expected);
+});
+
+it('wraps "Hello world" at 6 cols', () => {
+  const columns = 6;
+  const input = "Hello world";
+  const expected = "Hello \nworld";
+  const actual = columnWrap(input, columns);
+  expect(actual).toBe(expected);
+});
+
+it('wraps "Hello world" at 7 cols', () => {
+  const columns = 7;
+  const input = "Hello world";
+  const expected = "Hello \nworld";
+  const actual = columnWrap(input, columns);
+  expect(actual).toBe(expected);
+});
+
+it('wraps "Hello world" at 8 cols', () => {
+  const columns = 8;
+  const input = "Hello world";
+  const expected = "Hello \nworld";
+  const actual = columnWrap(input, columns);
+  expect(actual).toBe(expected);
+});
+
+it('wraps "Hello world" at 9 cols', () => {
+  const columns = 9;
+  const input = "Hello world";
+  const expected = "Hello \nworld";
+  const actual = columnWrap(input, columns);
+  expect(actual).toBe(expected);
+});
+
+it('wraps "Hello world" at 10 cols', () => {
+  const columns = 10;
+  const input = "Hello world";
+  const expected = "Hello \nworld";
+  const actual = columnWrap(input, columns);
+  expect(actual).toBe(expected);
+});
+
+it('wraps "Hello world" at 11 cols', () => {
+  const columns = 11;
+  const input = "Hello world";
+  const expected = "Hello world";
+  const actual = columnWrap(input, columns);
+  expect(actual).toBe(expected);
+});
+
+it('wraps "Hello world" at 12 cols', () => {
+  const columns = 12;
+  const input = "Hello world";
+  const expected = "Hello world";
+  const actual = columnWrap(input, columns);
+  expect(actual).toBe(expected);
+});
--- src/column-wrap.ts
+++ src/column-wrap.ts
@@ -1,19 +1,57 @@
-export function columnWrap(input: string, columns: number): string {
-  let currentCol = 0;
-  let cursor = 0;
+function breakLine(input: string, columns: number): [string, string] {
+  let cursor: number;
+  let currentChar: string;
+
+  const setCursor = (i: number) => (currentChar = input.charAt((cursor = i)));
+
+  const cursorIsAtWhitespace = () => /\s/.test(currentChar);
+  const cursorIsAtEnd = () => cursor >= input.length;
+
+  const advanceCursorToNextNonWhitespace = () => {
+    const search = /\S/.exec(input.substring(cursor));
+    if (search === null) {
+      return;
+    }
+    setCursor(cursor + search.index);
+  };
+
+  const reverseCursorToLastNonWord = () => {
+    const search = /(?:\w+(?!\w))+$/.exec(input.substring(0, cursor));
+    if (search === null || search.index === 0) {
+      return;
+    }
+    setCursor(search.index);
+  };
+
+  setCursor(columns);
+
+  if (cursorIsAtWhitespace()) {
+    advanceCursorToNextNonWhitespace();
+  } else if (!cursorIsAtEnd()) {
+    reverseCursorToLastNonWord();
+  }
+
+  return [input.substring(0, cursor), input.substring(cursor)];
+}
+
+function wrapLine(input: string, columns: number): string {
   let output = "";
   while (true) {
-    const currentChar = input.charAt(cursor++);
-    output += currentChar;
-    if (cursor >= input.length) {
+    const [before, after] = breakLine(input, columns);
+    output += before;
+    if (after === "") {
       break;
-    }
-    if (currentChar === "\n") {
-      currentCol = 0;
-    } else if (++currentCol >= columns) {
+    } else {
       output += "\n";
-      currentCol = 0;
+      input = after;
     }
   }
   return output;
+}
+
+export function columnWrap(input: string, columns: number): string {
+  return input
+    .split(/\n/g)
+    .map(line => wrapLine(line, columns))
+    .join("\n");
 }
